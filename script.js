939f39b73c02fe76b1564d39aba8c0599b98518b
sguo <sguo@expedia.com>
2020-12-03 22:12:30 -0600

    styles and refresh

--- script.js
+++ script.js
@@ -9,19 +9,26 @@ tableRef.innerHTML = "";
 const url = new URL(`https://api.covalenthq.com/v1/pricing/tickers/`);
 url.search = new URLSearchParams({
     key: APIKEY,
-    tickers: ["BTC", "WETH", "DAI", "YFI", "AAVE", "UNI"]
+    tickers: ["BTC", "WETH", "DOT", "YFI", "AAVE"]
 })
 
 // Use Fetch API to get Covalent data and display in token table
-fetch(url)
-.then((resp) => resp.json())
-.then(function(data) {
-    let tokens = data.data.items;
-    return tokens.map(function(token) { // Map through the results and for each run the code below
-    tableRef.insertRow().innerHTML = 
-        `<td><img src=${token.logo_url} style=width:50px;height:50px;></td>` +
-        `<td> ${token.contract_name} </td>` +
-        `<td> ${token.contract_ticker_symbol} </td>` +
-        `<td> $${parseFloat(token.quote_rate).toFixed(2)} </td>`
-    })
-})
+function getSpotPrices(url) {
+    fetch(url)
+        .then((resp) => resp.json())
+        .then(function(data) {
+            let tokens = data.data.items;
+            tableRef.innerHTML = "";
+            return tokens.map(function(token) { // Map through the results and for each run the code below
+                tableRef.insertRow().innerHTML =
+                    `<td><img src=${token.logo_url} class="token-logo"></td>` +
+                    `<td class="token"> ${token.contract_name} </td>` +
+                    `<td class="token"> ${token.contract_ticker_symbol} </td>` +
+                    `<td class="token"> $${parseFloat(token.quote_rate).toFixed(2)} </td>`
+            })
+        })
+}
+
+//Update the spot price every 30 seconds which is also the refresh rate of Covalent API
+getSpotPrices(url);
+setInterval(() => getSpotPrices(url), 30*1000);
