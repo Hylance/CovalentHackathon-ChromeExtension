8d3870efef690a62f49a071b6ebe16056ce42bc6
sguo <sguo@expedia.com>
2020-12-04 20:45:50 -0600

    add chrome storage

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const APIKEY = 'onemillionwallets';
 const tableRef = document.getElementById('tokenTable').getElementsByTagName('tbody')[0];
 tableRef.innerHTML = "";
 
-let initialTokens = ["BTC", "WETH", "AAVE", "YFI", "SNX"];
-localStorage.setItem('tokens', JSON.stringify(initialTokens));
+//let initialTokens = ["BTC", "WETH"];
+//localStorage.setItem('tokens', JSON.stringify(initialTokens));
 
 // Covalent API request setup
 const url = new URL(`https://api.covalenthq.com/v1/pricing/tickers/`);
@@ -17,10 +17,17 @@ document.getElementById("toggle").addEventListener("click", toggle);
 
 // Use Fetch API to get Covalent data and display in token table
 function getSpotPrices(url) {
-    url.search = new URLSearchParams({
-        key: APIKEY,
-        tickers: JSON.parse(localStorage.getItem('tokens'))
+
+    chrome.storage.sync.get('storedTokens', function(result) {
+        url.search = new URLSearchParams({
+            key: APIKEY,
+            tickers: result.storedTokens || []
+        })
+        fetchUrl(url)
     })
+}
+
+function fetchUrl(url) {
     fetch(url)
         .then((resp) => resp.json())
         .then(function(data) {
@@ -40,14 +47,12 @@ function addNewToken() {
     const newToken = document.getElementById("new-token").value;
     document.getElementById("new-token").value = "";
     saveNewTokenToLocalStorage(newToken.toUpperCase());
-    getSpotPrices(url);
 }
 
 function removeOldToken() {
     const oldToken = document.getElementById("old-token").value;
     document.getElementById("old-token").value = "";
     removeOldTokenFromLocalStorage(oldToken.toUpperCase());
-    getSpotPrices(url);
 }
 
 function toggle() {
@@ -60,26 +65,39 @@ function toggle() {
 }
 
 function saveNewTokenToLocalStorage(newToken) {
-    let tokens = [];
+    //let tokens = [];
     // Parse the serialized data back into an aray of objects
-    tokens = JSON.parse(localStorage.getItem('tokens')) || [];
+    //tokens = JSON.parse(localStorage.getItem('tokens')) || [];
     // Push the new data (whether it be an object or anything else) onto the array
-    tokens.push(newToken);
+    //tokens.push(newToken);
     // Re-serialize the array back into a string and store it in localStorage
-    localStorage.setItem('tokens', JSON.stringify(tokens));
+    //localStorage.setItem('tokens', JSON.stringify(tokens));
+    chrome.storage.sync.get('storedTokens', function(result) {
+        let storedTokens = result.storedTokens || [];
+        storedTokens.push(newToken);
+        chrome.storage.sync.set({'storedTokens': storedTokens}, function() {
+            getSpotPrices(url)
+        });
+    });
 }
 
 function removeOldTokenFromLocalStorage(oldToken) {
-    let tokens = [];
+    //let tokens = [];
     // Parse the serialized data back into an aray of objects
-    tokens = JSON.parse(localStorage.getItem('tokens')) || [];
+    //tokens = JSON.parse(localStorage.getItem('tokens')) || [];
     // Push the new data (whether it be an object or anything else) onto the array
-    const index = tokens.indexOf(oldToken);
-    if (index > -1) {
-        tokens.splice(index, 1);
-    }
+    chrome.storage.sync.get('storedTokens', function(result) {
+        let storedTokens = result.storedTokens || [];
+        const index = storedTokens.indexOf(oldToken);
+        if (index > -1) {
+            storedTokens.splice(index, 1);
+        }
+        chrome.storage.sync.set({'storedTokens': storedTokens}, function() {
+            getSpotPrices(url)
+        });
+    });
     // Re-serialize the array back into a string and store it in localStorage
-    localStorage.setItem('tokens', JSON.stringify(tokens));
+    //localStorage.setItem('tokens', JSON.stringify(tokens));
 }
 
 //Update the spot price every 10 seconds which is also the refresh rate of Covalent API
